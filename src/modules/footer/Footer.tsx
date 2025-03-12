27a62e7ab787464a77bcfcd604cdfb45a28c01b8
Taras Kovalyk <tarik2454@gmail.com>
2025-03-12 19:45:52 +0200

    feat: enhance Footer component with navigation and logo; update Container for style merging; remove unused Modal and PageWrapper components; add .DS_Store files

--- src/modules/footer/Footer.tsx
+++ src/modules/footer/Footer.tsx
@@ -1,9 +1,43 @@
 import Container from '@/shared/components/Container';
+import Logo from '../../assets/img/logo.png';
+import { menuItems } from '@/const/menuItems';
 
 export default function Footer() {
   return (
-    <Container>
-      <p>© 2025 Все права защищены</p>
-    </Container>
+    <footer className="pt-10 bg-secondaryBackground">
+      <Container>
+        <div className="flex flex-col justify-between">
+          <div className="grid grid-cols-[30%_70%] gap-[74px] mb-[57px]">
+            <h4 className="text-sm opacity-40">Быстрая навигация</h4>
+            <nav>
+              <ul className="flex flex-col gap-4">
+                {menuItems.map(item => (
+                  <li key={item.id}>
+                    <a href={item.link} className="font-medium opacity-67">
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+
+          <div className="w-full h-[1px] mb-[5] bg-primaryBackground opacity-30"></div>
+
+          <div className="flex flex-col">
+            <img
+              src={Logo}
+              alt="Логотип"
+              className="w-[165px] h-auto mb-[81px]"
+            />
+            <p className="mb-[25px] text-sm text-gray-400 ">
+              © 2025 Все права защищены
+            </p>
+
+            <div className="w-full h-[5px] bg-purpleBackground"></div>
+          </div>
+        </div>
+      </Container>
+    </footer>
   );
 }
--- src/shared/components/Container.tsx
+++ src/shared/components/Container.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface ContainerProps {
   children: ReactNode;
+  styles?: string;
 }
 
-export default function Container({ children }: ContainerProps) {
+export default function Container({ children, styles }: ContainerProps) {
   return (
-    <div className={`container mx-auto h-full px-4 xl:px-20`}>{children}</div>
+    <div className={twMerge(`container mx-auto h-full px-4 xl:px-20`, styles)}>
+      {children}
+    </div>
   );
 }
